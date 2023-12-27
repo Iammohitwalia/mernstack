e32a555d8badcfdcfda5bbd7f3c96cdbf45d4ff7
iammohitwalia <mohit.strixdevelopment@gmail.com>
2023-12-27 16:03:11 +0530

    ttest

--- backend/app.js
+++ backend/app.js
@@ -44,7 +44,7 @@ const order = require("./controller/order");
 const conversation = require("./controller/conversation");
 const message = require("./controller/message");
 const withdraw = require("./controller/withdraw");
-
+app.options('*', cors());
 app.use("/api/v2/user", user);
 app.use("/api/v2/conversation", conversation);
 app.use("/api/v2/message", message);
